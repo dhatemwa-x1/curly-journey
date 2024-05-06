3084230402ca3fee9ef0ad0c42beeba2f34d6150
Dhatemwa Abdulwahab <bazdate6@gmail.com>
2024-05-06 09:33:17 +0000

    home 2

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/my photo.jpg";
+import image from "../images/hom.jpg";
 
 const imageAltText = "Adult seated at a desk with a laptop and coffee cup.";
 
